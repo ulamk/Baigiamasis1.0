ecd93c548630d51f8208d37aef7f826f7de2ac95
ulamk <ulakiud@gmail.com>
2016-01-24 12:54:42 +0200

    Dis iz shit

---
+++ lolololol/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace lolololol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VisosDainosBoxas = new System.Windows.Forms.CheckedListBox();
+            this.PlaylistBoxas = new System.Windows.Forms.ListBox();
+            this.PažymėtiMygtukas = new System.Windows.Forms.Button();
+            this.PradėtiMygtukas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GrotiMygtukas = new System.Windows.Forms.Button();
+            this.StopMygtukas = new System.Windows.Forms.Button();
+            this.BuvusiMygtukas = new System.Windows.Forms.Button();
+            this.Shuffle = new System.Windows.Forms.CheckBox();
+            this.SekantiMygtukas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // VisosDainosBoxas
+            // 
+            this.VisosDainosBoxas.FormattingEnabled = true;
+            this.VisosDainosBoxas.Location = new System.Drawing.Point(12, 167);
+            this.VisosDainosBoxas.Name = "VisosDainosBoxas";
+            this.VisosDainosBoxas.Size = new System.Drawing.Size(156, 94);
+            this.VisosDainosBoxas.TabIndex = 0;
+            // 
+            // PlaylistBoxas
+            // 
+            this.PlaylistBoxas.FormattingEnabled = true;
+            this.PlaylistBoxas.Location = new System.Drawing.Point(174, 167);
+            this.PlaylistBoxas.Name = "PlaylistBoxas";
+            this.PlaylistBoxas.Size = new System.Drawing.Size(120, 95);
+            this.PlaylistBoxas.TabIndex = 1;
+            // 
+            // PažymėtiMygtukas
+            // 
+            this.PažymėtiMygtukas.Location = new System.Drawing.Point(12, 125);
+            this.PažymėtiMygtukas.Name = "PažymėtiMygtukas";
+            this.PažymėtiMygtukas.Size = new System.Drawing.Size(155, 23);
+            this.PažymėtiMygtukas.TabIndex = 2;
+            this.PažymėtiMygtukas.Text = "Pažymėti visas";
+            this.PažymėtiMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // PradėtiMygtukas
+            // 
+            this.PradėtiMygtukas.Location = new System.Drawing.Point(173, 125);
+            this.PradėtiMygtukas.Name = "PradėtiMygtukas";
+            this.PradėtiMygtukas.Size = new System.Drawing.Size(120, 23);
+            this.PradėtiMygtukas.TabIndex = 3;
+            this.PradėtiMygtukas.Text = "Pradėti";
+            this.PradėtiMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Visos dainos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pasirinktos dainos";
+            // 
+            // GrotiMygtukas
+            // 
+            this.GrotiMygtukas.Location = new System.Drawing.Point(15, 13);
+            this.GrotiMygtukas.Name = "GrotiMygtukas";
+            this.GrotiMygtukas.Size = new System.Drawing.Size(75, 23);
+            this.GrotiMygtukas.TabIndex = 6;
+            this.GrotiMygtukas.Text = "Groti";
+            this.GrotiMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // StopMygtukas
+            // 
+            this.StopMygtukas.Location = new System.Drawing.Point(97, 13);
+            this.StopMygtukas.Name = "StopMygtukas";
+            this.StopMygtukas.Size = new System.Drawing.Size(75, 23);
+            this.StopMygtukas.TabIndex = 7;
+            this.StopMygtukas.Text = "Stop";
+            this.StopMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // BuvusiMygtukas
+            // 
+            this.BuvusiMygtukas.Location = new System.Drawing.Point(15, 42);
+            this.BuvusiMygtukas.Name = "BuvusiMygtukas";
+            this.BuvusiMygtukas.Size = new System.Drawing.Size(75, 23);
+            this.BuvusiMygtukas.TabIndex = 8;
+            this.BuvusiMygtukas.Text = "Buvusi";
+            this.BuvusiMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // Shuffle
+            // 
+            this.Shuffle.AutoSize = true;
+            this.Shuffle.Location = new System.Drawing.Point(12, 102);
+            this.Shuffle.Name = "Shuffle";
+            this.Shuffle.Size = new System.Drawing.Size(58, 17);
+            this.Shuffle.TabIndex = 9;
+            this.Shuffle.Text = "Maišyti";
+            this.Shuffle.UseVisualStyleBackColor = true;
+            // 
+            // SekantiMygtukas
+            // 
+            this.SekantiMygtukas.Location = new System.Drawing.Point(97, 43);
+            this.SekantiMygtukas.Name = "SekantiMygtukas";
+            this.SekantiMygtukas.Size = new System.Drawing.Size(75, 23);
+            this.SekantiMygtukas.TabIndex = 10;
+            this.SekantiMygtukas.Text = "Sekanti";
+            this.SekantiMygtukas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 273);
+            this.Controls.Add(this.SekantiMygtukas);
+            this.Controls.Add(this.Shuffle);
+            this.Controls.Add(this.BuvusiMygtukas);
+            this.Controls.Add(this.StopMygtukas);
+            this.Controls.Add(this.GrotiMygtukas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PradėtiMygtukas);
+            this.Controls.Add(this.PažymėtiMygtukas);
+            this.Controls.Add(this.PlaylistBoxas);
+            this.Controls.Add(this.VisosDainosBoxas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox VisosDainosBoxas;
+        private System.Windows.Forms.ListBox PlaylistBoxas;
+        private System.Windows.Forms.Button PažymėtiMygtukas;
+        private System.Windows.Forms.Button PradėtiMygtukas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button GrotiMygtukas;
+        private System.Windows.Forms.Button StopMygtukas;
+        private System.Windows.Forms.Button BuvusiMygtukas;
+        private System.Windows.Forms.CheckBox Shuffle;
+        private System.Windows.Forms.Button SekantiMygtukas;
+    }
+}
+
